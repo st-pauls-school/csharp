e7f87285c91e17950a604883f2f4d4e50ce45162
Chris Harrison <cah@stpaulsschool.org.uk>
2018-09-19 11:50:51 +0100

    Moving to separate utils file

--- Polymorphism/ObjectLibrary/Person.cs
+++ Polymorphism/ObjectLibrary/Person.cs
@@ -2,20 +2,6 @@
 
 namespace ObjectLibrary
 {
-    public enum ChineseSign
-    {
-        Monkey, Rooster, Dog, Pig, Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Goat
-    }
-
-    public static class Utilities
-    {
-        public static ChineseSign ToChineseSign(this DateTime dob)
-        {
-            // todo: this still needs to consider the Chinese New Year isn't 1st January http://www.chinesenewyears.info/chinese-new-year-calendar.php  
-            return (ChineseSign)(dob.Year%12);
-        }
-    }
-
     /// <summary>
     /// The base person class 
     /// </summary>
---
+++ Polymorphism/PersonTests/UtilitiesTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PersonTests
+{
+    [TestClass]
+    public class UtilitiesTests
+    {
+        [TestMethod]
+        public void TestChineseSignLogic()
+        {
+            Assert.AreEqual(new DateTime(2008, 10, 1).ToChineseSign(), ChineseSign.Rat);
+            Assert.AreEqual(new DateTime(2012, 10, 1).ToChineseSign(), ChineseSign.Dragon);
+            Assert.AreEqual(new DateTime(2018, 1, 1).ToChineseSign(), ChineseSign.Rooster, "before Chinese New Year");
+        }
+    }
+}
