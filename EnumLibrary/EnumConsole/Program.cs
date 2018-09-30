0798a1061b38b91ca89b44bdd3105e6ab168f328
Chris Harrison <cah@stpaulsschool.org.uk>
2018-09-30 17:53:46 +0100

    Enum Library

---
+++ EnumLibrary/EnumConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnumLibrary;
+
+namespace EnumConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Suits c = Suits.Hearts;
+  //          bool isRed = (c & Suits.Red) == c;
+
+            Console.WriteLine("The card's suit is: {0}", c);
+            Console.WriteLine("The card is red: {0}", isRed);
+
+//            Console.WriteLine("Sp|Di: {0}", (Suits.Spades | Suits.Diamonds).ToString());
+            Console.Read();
+        }
+    }
+}
---
+++ EnumLibrary/EnumLibrary/Suits.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumLibrary
+{
+//    [Flags]
+    public enum Suits
+    {
+        Spades = 1,
+        Clubs = 2,
+        Diamonds = 4,
+        Hearts = 8, 
+
+//        Red = Diamonds | Hearts, 
+//        Black = Spades | Clubs 
+
+
+    }
+
+}
