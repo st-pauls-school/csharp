6aa6f60a1aa28fdce4693e6017fd40c73acb1ebe
Chris Harrison <cah@stpaulsschool.org.uk>
2018-11-30 09:18:06 +0000

    Recursion exercises

---
+++ Recursion/Recursion/RecursiveFunctions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace Recursion
+{
+    public class RecursiveFunctions
+    { 
+        
+
+        public static List<int> FirstNNaturalNumbers(int n)
+        {
+            if (n == 0)            
+                return new List<int>();
+            
+            List<int> li = FirstNNaturalNumbers(n - ((n > 0) ? 1 : -1));
+            li.Add(n);
+            return li;
+        }
+
+        public static List<int> Split(int n)
+        {
+            if (n <= 0)
+                return new List<int>();
+
+            List<int> li = Split(n/10);
+            li.Add(n%10);
+            return li;
+        }
+
+        public static List<int> Fibonacci(int n)
+        {
+            if (n <= 1)
+                return new List<int> { 1 };
+            if (n <= 2)
+                return new List<int> { 1, 1 };
+            List<int> li = Fibonacci(n-1);
+            li.Add(li[li.Count-1] + li[li.Count - 2]);
+            return li;
+        }
+        public static bool IsPrime(int n, int factor = 2)
+        {
+            if (n < 2)
+                return false;
+            if (factor >= n)
+                return true;
+            if (n % factor == 0)
+                return false;
+            return IsPrime(n, factor + 1);
+        }
+    }
+}
---
+++ Recursion/RecursiveTestProject/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Recursion;
+
+namespace RecursiveTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestFirstPositive()
+        {
+            CollectionAssert.AreEqual(new List<int> (), RecursiveFunctions.FirstNNaturalNumbers(0));
+            CollectionAssert.AreEqual(new List<int> { 1, 2, 3, 4, 5 }, RecursiveFunctions.FirstNNaturalNumbers(5));
+        }
+
+        [TestMethod]
+        public void TestFirstNegative()
+        {
+
+            // Extend your function to count negatively if the input was negative.
+            CollectionAssert.AreEqual(new List<int> { -1, -2, -3, -4, -5 }, RecursiveFunctions.FirstNNaturalNumbers(-5));
+        }
+
+        [TestMethod]
+        public void TestSplit()
+        {
+
+            // Write a function to return a list of the individual digits of a given integer.
+            CollectionAssert.AreEqual(new List<int> { 1, 2, 3, 4, 5 }, RecursiveFunctions.Split(12345));
+            CollectionAssert.AreEqual(new List<int> { 3, 7, 0, 9, 4 }, RecursiveFunctions.Split(37094));
+        }
+
+        [TestMethod]
+        public void TestPrime()
+        {
+
+            // Write a function that returns a Boolean saying whether a given integer is prime or not using recursion.
+            Assert.IsFalse(RecursiveFunctions.IsPrime(1));
+            Assert.IsFalse(RecursiveFunctions.IsPrime(100));
+            Assert.IsTrue(RecursiveFunctions.IsPrime(2));
+            Assert.IsTrue(RecursiveFunctions.IsPrime(17));
+
+            Assert.AreEqual(true, RecursiveFunctions.IsPrime(31));
+
+        }
+        [TestMethod]
+        public void TestFib()
+        {
+
+            //Write a function to return a list of the first n Fibonacci numbers for a given integer.
+            //Write it iteratively
+            //Write it recursively
+            CollectionAssert.AreEqual(new List<int> { 1, 1, 2, 3, 5, 8 }, RecursiveFunctions.Fibonacci(6));
+
+        }
+    }
+}
