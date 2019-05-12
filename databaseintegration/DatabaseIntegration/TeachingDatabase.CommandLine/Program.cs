528aeaaf63d90ff1d1e3bc3bf0c283b13556ebd7
Chris Harrison <cah@stpaulsschool.org.uk>
2019-05-12 16:39:01 +0100

    sqlite database integration

---
+++ databaseintegration/DatabaseIntegration/TeachingDatabase.CommandLine/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Data;
+
+namespace TeachingDatabase.CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionStringSettings cxnstring = ConfigurationManager.ConnectionStrings["sl"];
+            SQLiteConnection cxn = new SQLiteConnection(cxnstring.ConnectionString);
+            cxn.Open();
+            SQLiteCommand cmd = new SQLiteCommand();
+            cmd = cxn.CreateCommand();
+            cmd.CommandText = "select yg.name as 'Year', s.name as Student from YearGroups yg join StudentYearGroup syg left join Students s on syg.studentid = s.id where yg.name = 'L8' ";
+
+            SQLiteDataReader reader = cmd.ExecuteReader();
+
+            while(reader.Read())
+            {
+                Console.WriteLine("{0} - {1}", reader.GetString(0), reader.GetString(1));
+            }
+
+            cxn.Close();
+            Console.ReadKey();
+        }
+    }
+}
