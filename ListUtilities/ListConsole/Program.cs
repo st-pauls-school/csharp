3808802a1b94e007ac9c462de256f8ae65b66853
spscah <cah@stpaulsschool.org.uk>
2019-09-23 09:38:32 +0100

    Nasty list examples

---
+++ ListUtilities/ListConsole/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfIntegers = new List<int>() { 1, 2, 3, 4 };
+
+            Double(listOfIntegers);
+
+
+            foreach (int i in listOfIntegers)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+
+            listOfIntegers.Add(10);
+            listOfIntegers.Add(123);
+
+            // listOfIntegers.Clear();
+
+            
+            Console.ReadKey();
+
+            listOfIntegers.Insert(0, -15);
+
+            foreach (int i in listOfIntegers)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+
+            listOfIntegers[0] -= 10;
+
+            if(listOfIntegers.Contains(10))
+                listOfIntegers.Remove(10);
+            listOfIntegers.RemoveAt(0);
+            
+
+
+        }
+
+        static int Double(int x)
+        {
+            x *= 2;
+            return x;
+        }
+
+        static void Double(List<int> l)
+        {
+            for (int i = 0; i < l.Count; i++)
+                l[i] *= 2;
+        }
+    }
+}
---
+++ ListUtilities/ListFunctions/ListUtilitiies.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListFunctions
+{
+    public class ListUtilitiies
+    {
+        public static void ExampleMethod()
+        {
+            int[] arrayOfIntegers = new int[10];
+            List<int> listOfIntegers = new List<int>();
+            listOfIntegers.Add(10);
+            
+
+            
+
+        }
+    }
+}
