27605461f84a09173752b7716cbc9f652adb1d4a
Chris Harrison <cah@stpaulsschool.org.uk>
2019-10-05 22:19:33 +0100

    Binary file reading

---
+++ BinaryFiles/BinaryFiles/BinaryConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] logo = File.ReadAllBytes(Properties.Settings.Default.filename);
+            byte first = logo[0];
+            byte second = logo[1];
+
+            int res1 = first & (byte)'B';
+            int res2 = second & (byte)'M';
+
+
+            Console.ReadKey();
+        }
+    }
+}
