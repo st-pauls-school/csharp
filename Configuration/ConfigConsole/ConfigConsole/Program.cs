d3eb9735b4d4f33fc4c14df407322b96122abb96
Chris Harrison <cah@stpaulsschool.org.uk>
2019-10-05 21:46:26 +0100

    Basic configuration options

---
+++ Configuration/ConfigConsole/ConfigConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = Properties.Settings.Default.MySettingValue;
+            string val = Properties.Settings.Default.ChangableSetting;
+            Properties.Settings.Default.ChangableSetting = "new value";
+            Properties.Settings.Default.MySettingValue = 76;
+
+            ConfigurationManager.AppSettings[key];
+        }
+    }
+}
