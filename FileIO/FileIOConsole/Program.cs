5c42f8b8789536219d9b21508782cc7f2c232b2b
spscah <cah@stpaulsschool.org.uk>
2019-10-07 08:26:57 +0100

    FileIO

---
+++ FileIO/FileIOConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fn = "textfile.txt";
+
+            bool present = File.Exists(fn);
+
+            if (present)
+            {
+                string contents;
+                using (StreamReader sr = new StreamReader(fn))
+                {
+                    while((contents = sr.ReadLine()) != null)                   
+                    {
+                        Console.WriteLine(contents);
+
+                    }
+                }
+            }
+            Console.ReadKey();
+
+            using (StreamWriter sw = new StreamWriter("outputfile.txt", true))
+            {
+                sw.WriteLine("first line - another new");
+                sw.WriteLine("second line");
+                sw.WriteLine("third line");
+            }
+
+        }
+    }
+}
