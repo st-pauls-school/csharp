860cd70e32b53825d2a28c3820c4321d8a1ea753
Chris Harrison <cah@stpaulsschool.org.uk>
2019-11-08 19:37:22 +0000

    LookSay - single thread

---
+++ LookSay/LookSay/LookSay.App/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace LookSay.App
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Seed? ");
+            string seed = Console.ReadLine();
+            long ms = -1;
+            if (int.TryParse(seed, out int iSeed))
+            {
+                Sequence s = new Sequence(iSeed);
+                Console.Write("How many? ");
+                string howMany = Console.ReadLine();
+                if (int.TryParse(howMany, out int index))
+                {
+                    using(StreamWriter sw = new StreamWriter("output.txt"))
+                    {
+                        Stopwatch timer = new Stopwatch();
+                        timer.Start();
+                        for (int i = index-3; i <= index; i++)
+                        {
+                            string res = s.Generate(i);
+                            sw.WriteLine("[{0}]: {1} ({2})", i, res, res.Length);
+                            ms = timer.ElapsedMilliseconds;
+                            string sf = $"clock: {i} in {ms}ms";
+                            sw.WriteLine(sf);
+                            Console.WriteLine(sf);
+                        }
+                        timer.Stop();
+                    }
+                }
+            }
+
+            Console.WriteLine("done, {0}ms", ms);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LookSay/LookSay/LookSay/Sequence.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LookSay
+{
+    public class Sequence
+    {
+        readonly IList<string> _cache;
+        
+
+        public Sequence(int seed)
+        {
+            _cache = new List<string> { seed.ToString()};
+        }
+
+        public string Generate(int number)
+        {
+            string result = _cache[0];
+            for (int i = 0; i < number; i++)
+                result = MakeNext(result);
+            return result;
+        }
+        
+        public IList<string> GenerateList(int number)
+        {
+            while (_cache.Count < number)
+            {
+                string last = _cache.Last();
+                _cache.Add(MakeNext(last));
+            }
+
+            return _cache.Take(number).ToList().AsReadOnly();
+        }
+
+        string MakeNext(string incoming)
+        {
+            StringBuilder sb = new StringBuilder();
+            char previous = (char)0;
+            uint counter = 0;
+            foreach (char c in incoming)
+            {
+                if (c == previous)
+                {
+                    counter++;
+                    continue;
+                }
+
+                if (counter > 0)
+                {
+                    sb.Append(counter);
+                    sb.Append(previous);
+                }
+
+                previous = c;
+                counter = 1;
+            }
+            sb.Append(counter);
+            sb.Append(previous);
+
+            return sb.ToString();
+
+        }
+    }
+}
