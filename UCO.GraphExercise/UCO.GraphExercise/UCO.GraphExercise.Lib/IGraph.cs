b4ef1fda5a3b37dac68d3005996fcfd378aeb863
Chris Harrison <cah@stpaulsschool.org.uk>
2019-12-02 10:33:33 +0000

    BMO2019 & UCO Graph Exercise

---
+++ UCO.GraphExercise/UCO.GraphExercise/UCO.GraphExercise.Lib/IGraph.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace UCO.GraphExercise.Lib
+{
+    public interface IGraph<T> 
+    {
+        bool Connected { get; }
+        bool Cyclic { get; }
+        bool Directed { get; }
+        bool Weighted { get; }
+
+        int ShortestPath(IVertex<T> v1, IVertex<T> v2);
+        IVertex<T> CentreOfTheGraph { get; }
+        IGraph<T> MinimumSpanningTree(MinimalAlgorithms algm);
+        IList<IVertex<T>> Traversal(TreeTraversalMethods direction);
+        IList<IVertex<T>> DeletionOrder { get; }
+    }
+    public interface IVertex<T>
+    {
+
+    }
+
+    public enum TreeTraversalMethods
+    {
+        BreadthFirst, DepthFirst
+    }
+
+    public enum MinimalAlgorithms
+    {
+        Prim, Kruskal
+    }
+
+}
