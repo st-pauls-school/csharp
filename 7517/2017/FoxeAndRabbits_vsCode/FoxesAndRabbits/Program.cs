b0610125ff1d374dcf551135644b2db1f25b8080
Chris Harrison <cah@stpaulsschool.org.uk>
2021-06-15 21:20:54 +0100

    Real Section D

---
+++ 7517/2017/FoxeAndRabbits_vsCode/FoxesAndRabbits/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FoxesAndRabbits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InternalQuestions.InternalQuestionsProgram.InternalQuestionsMain(args);
+        }
+    }
+}
